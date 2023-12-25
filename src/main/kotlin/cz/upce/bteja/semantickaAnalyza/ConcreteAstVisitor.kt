717dcea422c16ad3bcec6b9573c3aa8269fcdbe7
Tomáš Kvapil <tomaskvapil5@gmail.com>
2023-12-25 13:06:57 +0100

    semanticka analyza: interface AstVisitor a jeho implementace

---
+++ src/main/kotlin/cz/upce/bteja/semantickaAnalyza/ConcreteAstVisitor.kt
@@ -0,0 +1,99 @@
+package cz.upce.bteja.semantickaAnalyza
+
+class ConcreteAstVisitor : AstVisitor<Unit> {
+    override fun visit(moduleNode: ModuleNode) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(variableExpression: VariableExpression) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(functionCall: FunctionCall) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(identifierExpression: IdentifierExpression) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(arrayAccess: ArrayAccess) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(binaryOperatorExpression: BinaryOperatorExpression) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(unaryOperatorExpression: UnaryOperatorExpression) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(statementSequence: StatementSequence) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(assignment: Statement.Assignment) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(ifStatement: Statement.IfStatement) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(elsifStatement: Statement.ElsifStatement) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(procedureDeclaration: ProcedureDeclaration) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(parameter: Parameter) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(identifier: Identifier) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(operator: Operator) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(literal: Literal) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(designator: Designator) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(selector: Selector) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(literalExpression: LiteralExpression) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(unaryExpression: UnaryExpression) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(declarationSequence: DeclarationSequence) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(constDeclaration: ConstDeclaration) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(variableDeclaration: VariableDeclaration) {
+        TODO("Not yet implemented")
+    }
+
+    override fun visit(binaryExpression: BinaryExpression) {
+        TODO("Not yet implemented")
+    }
+}
